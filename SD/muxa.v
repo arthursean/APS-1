368b08e7ffca3e5c1747330e6e93309faba626b3
Arthur Sean C. Campos <113319924+arthursean@users.noreply.github.com>
2025-06-07 15:59:35 -0300

    Update muxa.v

--- SD/muxa.v
+++ SD/muxa.v
@@ -2,11 +2,12 @@
 `include "not.v"
 `include "and.v"
 `include "xor.v"
-module xora(output wire F, input wire A, B, C, D, [1:0]sel);
+`timescale 1ns/1ps
+module mux_4t1a(output wire F, input wire A, B, C, D, input wire [1:0]Sel);
 wire inv_s0, inv_s1; 
 wire is_A, is_B, is_C, is_D;
-wire res_A, res_B, res_C, res_D
-wire U_AB, U_ABC
+wire res_A, res_B, res_C, res_D;
+wire U_AB, U_ABC;
 nota(.F(inv_s0), .A(sel[0]));
 nota(.F(inv_s1), .A(sel[1]));
 anda(.F(is_A), .A(inv_s1), .B(inv_s0));
@@ -20,4 +21,18 @@ anda(.F(res_D), .A(D), .B(is_D));
 ora(.F(U_AB), .A(res_A), .B(res_B));
 ora(.F(U_ABC), .A(U_AB), .B(res_C));
 ora(.F(F), .A(U_ABC), .B(res_D));
+endmodule
+
+`timescale 1ns/1ps
+
+module mux_4t1b
+	(output wire F,
+	input wire A, B, C, D,
+	input wire [1:0] Sel);
+
+	assign F = (Sel == 2'b00) ? A :
+			   (Sel == 2'b01) ? B :
+			   (Sel == 2'b10) ? C :
+			   (Sel == 2'b11) ? D :
+			   1'bX;
 endmodule
